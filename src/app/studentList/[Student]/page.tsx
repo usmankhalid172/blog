ba53ec65d8b1be1ec68edf6cd00d9bc715442b78
usmankhalid172 <usmankhalid17248@gmail.com>
2023-09-11 11:11:41 +0500

    got it

--- src/app/studentList/[Student]/page.tsx
+++ src/app/studentList/[Student]/page.tsx
@@ -4,7 +4,7 @@ export default function StudentList({params}){
     return(
         <div>
         <h1>Studetn Details</h1>
-        <h3>Name : {params}</h3>
+        <h3>Name : {params.Student}</h3>
         </div>
     )
 }
